8c267152b8584bfdcbebc8bda69a1a693be3bdf3
DiegoDavid17 <diegodavidalmiron@gmail.com>
2022-04-10 02:36:17 -0300

    se agregaron las restricciones a la sección mis proyectos

--- src/app/components/seccion-mis-proyectos/seccion-mis-proyectos.component.ts
+++ src/app/components/seccion-mis-proyectos/seccion-mis-proyectos.component.ts
@@ -4,6 +4,7 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
 import { ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from "../../servicios/auth.service";
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-seccion-mis-proyectos',
@@ -35,13 +36,13 @@ export class SeccionMisProyectosComponent implements OnInit {
     });
 
     this.formProyectoEdit= this.formBuilder.group({
-      nombreProyecto: '',
-      descripcionProyecto: ''
+      nombreProyecto: ['', [Validators.required]],
+      descripcionProyecto: ['', [Validators.required]],
    })
 
    this.formProyectoAdd= this.formBuilder.group({
-    nombreProyecto: '',
-    descripcionProyecto: ''
+    nombreProyecto: ['', [Validators.required]],
+    descripcionProyecto: ['', [Validators.required]],
     })
 
   this.formProyectoDelete= this.formBuilder.group({
@@ -57,12 +58,7 @@ export class SeccionMisProyectosComponent implements OnInit {
 
   // CRUD DE PROYECTOS
 
-  editarProyecto(item:number){
-    this.closebuttonEditarProyecto.nativeElement.click();
-    this.miPortfolio.MisProyectos[item].nombreProyecto=this.formProyectoEdit.value.nombreProyecto;
-    this.miPortfolio.MisProyectos[item].descripcionProyecto=this.formProyectoEdit.value.descripcionProyecto;
-    alert('Se editó el proyecto: "'+ this.miPortfolio.MisProyectos[item].nombreProyecto + '"');
-  }
+  // Método para mostrar proyecto
 
   nombreProyectoSelect: string = '';
   descripcionProyectoSelect: string = '';
@@ -73,6 +69,31 @@ export class SeccionMisProyectosComponent implements OnInit {
     this.descripcionProyectoSelect=this.miPortfolio.MisProyectos[this.auxIndex].descripcionProyecto;
   } 
 
+  //Getters de agregar proyecto
+
+  get NombreProyecto(){
+    return this.formProyectoAdd.get("nombreProyecto");
+   }
+
+   get DescripcionProyecto(){
+    return this.formProyectoAdd.get("descripcionProyecto");
+   }
+
+  //Funciones para agregar proyecto
+
+  enviarProyectoAdd(event: Event){
+    // Detenemos la propagación o ejecución del compotamiento submit de un form
+    event.preventDefault; 
+ 
+    if (this.formProyectoAdd.valid){
+      // Llamamos a nuestro servicio para enviar los datos al servidor
+      // También podríamos ejecutar alguna lógica extra
+      this.agregarProyecto();
+    }else{
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+      this.formProyectoAdd.markAllAsTouched(); 
+    }
+  }
 
   nombreDeProyecto: string = "";
   descripcionDeProyecto: string = "";
@@ -84,12 +105,73 @@ export class SeccionMisProyectosComponent implements OnInit {
     this.descripcionDeProyecto= this.formProyectoAdd.value.descripcionProyecto;
 
     this.miPortfolio.MisProyectos.push({nombreProyecto: this.nombreDeProyecto, descripcionProyecto: this.descripcionDeProyecto});
-    alert('Se agregó un nuevo proyecto a tu lista');
+    Swal.fire({
+      icon: 'success',
+      title: 'Se agregó el proyecto: "'+ this.nombreDeProyecto + '"',
+      showConfirmButton: false,
+      timer: 4000
+    })
   }
+
+  //Métodos para editar proyectos
+
+  get NombreProyectoEdit(){
+    return this.formProyectoEdit.get("nombreProyecto");
+   }
+
+   get DescripcionProyectoEdit(){
+    return this.formProyectoEdit.get("descripcionProyecto");
+   }
+
+  //Funciones para editar proyecto
+
+  enviarProyectoEdit(event: Event, item: number){
+    // Detenemos la propagación o ejecución del compotamiento submit de un form
+    event.preventDefault; 
+ 
+    if (this.formProyectoEdit.valid){
+      // Llamamos a nuestro servicio para enviar los datos al servidor
+      // También podríamos ejecutar alguna lógica extra
+      this.editarProyecto(item);
+    }else{
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+      this.formProyectoEdit.markAllAsTouched(); 
+    }
+  }
+
+  editarProyecto(item:number){
+    this.closebuttonEditarProyecto.nativeElement.click();
+
+    if(this.miPortfolio.MisProyectos[item].nombreProyecto==this.formProyectoEdit.value.nombreProyecto
+      && this.miPortfolio.MisProyectos[item].descripcionProyecto==this.formProyectoEdit.value.descripcionProyecto){
+        Swal.fire({
+          icon: 'info',
+          title: 'Sin cambios!!!',
+          showConfirmButton: false,
+          timer: 4000
+    })
+    }else{
+      Swal.fire({
+        icon: 'success',
+        title: 'Se editó el proyecto: "'+ this.miPortfolio.MisProyectos[item].nombreProyecto + '"',
+        showConfirmButton: false,
+        timer: 4000
+    })
+    this.miPortfolio.MisProyectos[item].nombreProyecto=this.formProyectoEdit.value.nombreProyecto;
+    this.miPortfolio.MisProyectos[item].descripcionProyecto=this.formProyectoEdit.value.descripcionProyecto;
+    }
+  }
+
+  //Funcion para eliminar proyecto
 
   eliminarProyecto(indice: number){
     this.closebuttonEliminarProyecto.nativeElement.click();
-    alert('Se eliminó el proyecto: "'+ this.miPortfolio.MisProyectos[indice].nombreProyecto + '"');
+    Swal.fire({
+      icon: 'success',
+      title: 'Se eliminó el proyecto: "'+ this.miPortfolio.MisProyectos[indice].nombreProyecto + '"',
+      showConfirmButton: false,
+      timer: 4000
+    })
     this.miPortfolio.MisProyectos.splice(indice, 1);
   }
 
